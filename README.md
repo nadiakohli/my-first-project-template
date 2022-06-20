b02ed7d87176b37e37009b85168f3747b200e508
nadiakohli <andruknadia@gmail.com>
2022-06-20 22:04:06 +0300

    do task

---
+++ README.md
@@ -0,0 +1,7 @@
+# My first project template :blush:
+
+## How to start work with project:
+
+- `npm install`
+
+- `npm start`
---
+++ src/app.js
@@ -0,0 +1,25 @@
+const a = 1;
+
+const foo = (name) => {
+  const lastName = name;
+  return lastName;
+};
+
+foo('Denis');
+
+const x = 200;
+
+console.log(window);
+console.log(x, a);
+
+const greet = (name) => `Hello ${name}`;
+greet('Anna');
+
+alert('Hello world');
+
+const obj = {
+  name: 'Karol',
+};
+
+const obj1 = obj.name;
+console.log(obj1);
